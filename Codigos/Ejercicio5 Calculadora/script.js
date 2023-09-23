b143511d255aea53e91f26363978e8b5eeb119f3
O22202 <O22202@lab.aca>
2023-09-23 12:22:49 -0500

    Ejercicio de Calculadora WIP

---
+++ Codigos/Ejercicio5 Calculadora/script.js
@@ -0,0 +1,51 @@
+
+let guardado1=0;
+let pantalla="";
+let opeador="nada";
+
+function btn1(){
+    pantalla = pantalla + "1";
+    document.getElementById("pantalla").value = pantalla;
+}
+
+function btn2(){
+    pantalla = pantalla + "2"
+    document.getElementById("pantalla").value = pantalla;
+}
+
+function btn3(){
+    pantalla = pantalla + "3"
+    document.getElementById("pantalla").value = pantalla;
+}
+
+function btn4(){
+    pantalla = pantalla + "4"
+    document.getElementById("pantalla").value = pantalla;
+}
+function btn5(){
+    pantalla = pantalla + "5"
+    document.getElementById("pantalla").value = pantalla;
+}
+
+function btn6(){
+    pantalla = pantalla + "6"
+    document.getElementById("pantalla").value = pantalla;
+}
+function btn7(){
+    pantalla = pantalla + "7"
+    document.getElementById("pantalla").value = pantalla;
+}
+
+function btn8(){
+    pantalla = pantalla + "8"
+    document.getElementById("pantalla").value = pantalla;
+}
+function btn9(){
+    pantalla = pantalla + "9"
+    document.getElementById("pantalla").value = pantalla;
+}
+
+function btn0(){
+    pantalla = pantalla + "0"
+    document.getElementById("pantalla").value = pantalla;
+}
