aeae7154ab589b0db9503e65f97a4bebb199de01
Oscar La Torre <oscarlatorre02@gmail.com>
2023-10-07 12:26:03 -0500

    Mas ejercicios y cambios

--- Codigos/07. React Next Js/myapp/src/app/alumnos/Alumnos.jsx
+++ Codigos/07. React Next Js/myapp/src/app/alumnos/Alumnos.jsx
@@ -1,29 +1,30 @@
-import { useState } from 'react'
+'use client'
+
+import { useState, useEffect } from 'react'
 
 import styles from './alumnos.module.css'
 import Formulario from './Formulario/Formulario.jsx'
 const Alumnos = () => {
 
     const [showFormulario, setShowFormulario ] = useState(false);
+    const [alumnos, setAlumnos] = useState([]);
+
+    useEffect(() => {
+        let alumnosInicial = JSON.parse(localStorage.getItem("alumnos"));
+        setAlumnos(alumnosInicial)
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem("alumnos", JSON.stringify(alumnos))
+    },[alumnos])
 
     const handleShowFormulario = () => {
         setShowFormulario(!showFormulario);
     }
 
-    const alumnos = [
-        {
-            id: 1,
-            nombre: "Juan",
-            apellido: "Perez",
-            carrera: "Sistemas"
-        },
-        {
-            id: 2,
-            nombre: "Gabriel",
-            apellido: "Sanchez",
-            carrera: "Derecho"
-        }
-    ]
+    const handleAgregar = (alumno) => {
+        setAlumnos([...alumnos, alumno])
+    }
 
     return (
         <>
@@ -40,7 +41,7 @@ const Alumnos = () => {
                 </thead>
                 <tbody>
                     {alumnos.map(item => {
-                        return (<tr>
+                        return (<tr key={item.id}>
                             <td>{item.id}</td>
                             <td>{item.nombre}</td>
                             <td>{item.apellido}</td>
@@ -54,7 +55,7 @@ const Alumnos = () => {
             <button onClick={handleShowFormulario}>AGREGAR</button>
             <br/>
             {
-                showFormulario && <Formulario />
+                showFormulario && <Formulario onEnviar={handleAgregar}/>
             }
             
         </>
